349abbbced87d3f0f2de804932718f02b60d4aa6
gitgtama <74250079+gitgtama@users.noreply.github.com>
2026-04-11 07:56:33 +0700

    Update get.php

--- gallery/get.php
+++ gallery/get.php
@@ -1,5 +1,5 @@
 <?php
-require '../config.php';
+require dirname(__DIR__) . '/config.php';
 
 $stmt = $pdo->query("SELECT * FROM projects ORDER BY id DESC");
 $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
