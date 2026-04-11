b97d00ad7ee2122a95a10686d7b983bcf6af2ee8
gitgtama <74250079+gitgtama@users.noreply.github.com>
2026-04-11 18:27:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
 
+date_default_timezone_set("Asia/Jakarta");
+
 if (isset($_POST['submit'])) {
     file_get_contents(
         'https://api.telegram.org/bot8684138011:AAEBYIv60HQHhmg7KVMI6JUw1LOidHfax6o/sendMessage' .
         '?' . http_build_query([
             'chat_id' => '8132536867',
             'text' => sprintf(
-                "--Kontak Baru\n\nNama: %s\nEmail: %s\nWhatsapp: %s\nPesan: %s",
+                "--%s\n\nNama: %s\nEmail: %s\nWhatsapp: %s\n\n%s", date('d M Y H:i:s'),
                 htmlspecialchars($_POST['name']),htmlspecialchars($_POST['email']),
                 htmlspecialchars($_POST['phone']), htmlspecialchars($_POST['message'])
             )
